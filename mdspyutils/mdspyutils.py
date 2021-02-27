c9d2acfd080b39fc4bc18e69e766cd56bc326ef6
kamal26 <kamal26@student.ubc.ca>
2021-02-26 20:15:39 -0500

    Docstring for function 1

--- mdspyutils/mdspyutils.py
+++ mdspyutils/mdspyutils.py
@@ -10,7 +10,7 @@ def parse_lab(file_name: str) -> list:
             search for all labs in the working directory based on the file
             extension.
     Returns:
-        list: each element of list is a content of one markdown cell.
+        list: Each element of list is a content of one markdown cell.
 
     Example:
         parse_lab()
